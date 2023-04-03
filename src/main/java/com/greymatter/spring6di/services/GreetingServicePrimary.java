081108de73c2e58936d8bb7ee267a9621a158628
Jorge García <jcgarciamed@gmail.com>
2023-04-02 20:39:26 -0500

    Primary Beans

--- src/main/java/com/greymatter/spring6di/services/GreetingServicePrimary.java
+++ src/main/java/com/greymatter/spring6di/services/GreetingServicePrimary.java
@@ -1,11 +1,13 @@
 package com.greymatter.spring6di.services;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
+@Primary
 @Service
 public class GreetingServicePrimary implements GreetingService {
     @Override
     public String sayGreeting() {
-        return "Hello From the Prim";
+        return "Hello From the Primary Bean";
     }
 }
