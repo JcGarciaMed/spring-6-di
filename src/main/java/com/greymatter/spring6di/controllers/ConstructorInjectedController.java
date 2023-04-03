76a506af22f975abb9805c1aa52cf8500c184acf
Jorge García <jcgarciamed@gmail.com>
2023-04-02 20:53:40 -0500

    Qualifiers

--- src/main/java/com/greymatter/spring6di/controllers/ConstructorInjectedController.java
+++ src/main/java/com/greymatter/spring6di/controllers/ConstructorInjectedController.java
@@ -1,13 +1,14 @@
 package com.greymatter.spring6di.controllers;
 
 import com.greymatter.spring6di.services.GreetingService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ConstructorInjectedController {
     private final GreetingService greetingService;
 
-    public ConstructorInjectedController(GreetingService greetingService) {
+    public ConstructorInjectedController(@Qualifier("greetingServiceImpl") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
---
+++ src/main/java/com/greymatter/spring6di/services/GreetingServiceSetterInjection.java
@@ -0,0 +1,12 @@
+package com.greymatter.spring6di.services;
+
+import org.springframework.stereotype.Service;
+
+@Service("setterGreetingBean")
+public class GreetingServiceSetterInjection implements GreetingService {
+
+    @Override
+    public String sayGreeting() {
+        return "Hey im setting a greeting!!!";
+    }
+}
