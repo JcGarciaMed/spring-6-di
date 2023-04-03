b0cfb4790984f5adb5e5559460f5ec6b54154832
Jorge García <jcgarciamed@gmail.com>
2023-04-02 20:35:18 -0500

    Dependency Injection Using Spring

--- src/main/java/com/greymatter/spring6di/services/GreetingServicePrimary.java
+++ src/main/java/com/greymatter/spring6di/services/GreetingServicePrimary.java
@@ -3,9 +3,9 @@ package com.greymatter.spring6di.services;
 import org.springframework.stereotype.Service;
 
 @Service
-public class GreetingServiceImpl implements GreetingService {
+public class GreetingServicePrimary implements GreetingService {
     @Override
     public String sayGreeting() {
-        return "Hello a todos desde el Service";
+        return "Hello From the Prim";
     }
 }
